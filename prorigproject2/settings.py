41068ac39b161502a3adc3dee64c6c5dfe5bb7b7
johnokwute <134557852+johnokwute@users.noreply.github.com>
2023-12-30 14:54:32 +0100

    Update settings.py

--- prorigproject2/settings.py
+++ prorigproject2/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'prorigdb2',
-        'USER' : 'prorigdb2',
+        'USER' : 'proriguser',
         'PASSWORD': '0000',
         'HOST': 'localhost'
     }
