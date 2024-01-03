704d94205d590190ca0c490d9f12796e56ef7061
johnokwute <134557852+johnokwute@users.noreply.github.com>
2024-01-03 08:03:15 +0100

    Update settings.py

--- prorigproject2/settings.py
+++ prorigproject2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^rtiyeq^k_j13eenk5zac$v+g_h_5oe4lw0d1ndb%lq=yt^3h-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['142.93.181.55']
+ALLOWED_HOSTS = ['174.138.79.13']
 
 
 # Application definition
